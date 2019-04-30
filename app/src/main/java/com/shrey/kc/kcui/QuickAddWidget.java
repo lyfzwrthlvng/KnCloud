2b1081a1cc1e90729511ca2f59af415078f907b9
Shrey <shrey.malik@flipkart.com>
2019-05-01 02:33:10 +0530

    Some placeholder for widget code

--- app/src/main/java/com/shrey/kc/kcui/QuickAddWidget.java
+++ app/src/main/java/com/shrey/kc/kcui/QuickAddWidget.java
@@ -1,8 +1,10 @@
 package com.shrey.kc.kcui;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.widget.RemoteViews;
@@ -17,8 +19,11 @@ public class QuickAddWidget extends AppWidgetProvider {
                                 int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
+        Intent addKnowledgeIntent = new Intent(context, LoggedInAddKnowledge.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, addKnowledgeIntent, 0);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.quick_add_widget);
+        //views.setOnClickPendingIntent(R.id.button_add_quick, pendingIntent);
         //views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
@@ -36,6 +41,7 @@ public class QuickAddWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
