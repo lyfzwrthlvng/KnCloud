60895863fa0d606752e9366c7cdde65dbe545b3e
Shrey <shrey.malik@flipkart.com>
2019-05-15 12:12:07 +0530

    added a better splash screen

---
+++ app/src/main/java/com/shrey/kc/kcui/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.shrey.kc.kcui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+        finish();
+    }
+}
